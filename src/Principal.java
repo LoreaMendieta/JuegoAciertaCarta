4ca07194a67330769959215f4535e91da3e6bae0
maitane <irakaslemai@gmail.com>
2019-12-17 16:12:31 +0100

    cartas editado

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Principal {
+	
 	public static void main(String[] args) {
 		//Programa Principal
 		 
+		List<Carta> listado = new ArrayList<Carta>();
+		listado.add(new Carta(3, "Oros"));
+		listado.add(new Carta(7, "Copas"));
+		listado.add(new Carta(1, "Bastos"));
+		
 	}
 }
